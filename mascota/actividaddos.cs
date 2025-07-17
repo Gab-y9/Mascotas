c46f350806997199be19a5c3f0a0d34a8ddc9a26
gaby <gonzlezgabriela6@gmail.com>
2025-07-17 13:03:47 -0600

    Create actividaddos.cs

---
+++ mascota/actividaddos.cs
@@ -0,0 +1,24 @@
+        public void EdadHumana(int edad)
+        {
+            Console.WriteLine(" Su edad humana es de " + " años " );
+        }
+        Total=edad*7
+            class program
+        {
+
+            static void Main(string[] args)
+            {  
+                info.EdadHumana (total)
+                m.MostrarInfo();
+                Mascota m=new Mascota(" chispita ",2, "perro");
+                info.nombre();
+                info.tipo();
+                info.edad();
+                info.sonido();
+                info.edadHumana();
+
+               
+                Console.WriteLine("Nueva edad")
+    }
+
+}
